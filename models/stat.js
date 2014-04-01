769e7c9354d72a03a9200709b375de63d1fadd6c
Rowan Oulton <rowan.oulton@gmail.com>
2014-04-01 22:37:26 +1300

    Update stat mongoose model No longer require percentile (will calc on fly in case of changes) Also adds support for key & type specification. Keys relate to data, eg "price" or "price per bedroom". Types relate to types of statistics, eg "mean", "median"

--- models/stat.js
+++ models/stat.js
@@ -10,9 +10,14 @@ var mongoose = require('mongoose'),
  */
 statSchema = new Schema({
     _suburb: { type: Number, ref: 'Suburb' },
+
+    /* A number representing a type of data (eg. "price", "price_per_room") */
+    key: Number,
+
+    /* A number representing a type of statistic (eg. mean, median, volume) */
     type: Number,
+
     value: Number,
-    percentile: Number,
     date: { type: Date, default: Date.now }
 });
 
