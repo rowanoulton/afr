0c24d0150e7198c2e19ddbdcd52ffd512c9feceb
Rowan Oulton <rowan.oulton@gmail.com>
2014-04-03 21:37:11 +1300

    Kill superflous whitespace in stat schema

--- models/stat.js
+++ models/stat.js
@@ -13,10 +13,8 @@ statSchema = new Schema({
     _region: { type: Number, ref: 'Region' },
     /* A number representing a type of data (eg. "price", "price_per_room") */
     key: Number,
-
     /* A number representing a type of statistic (eg. mean, median, volume) */
     type: Number,
-
     value: Number,
     date: { type: Date, default: Date.now }
 });
