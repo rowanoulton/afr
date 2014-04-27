fe7a3d3e66227c5a306816a3dd7283012ababe45
Rowan Oulton <rowan.oulton@gmail.com>
2014-04-27 18:42:35 +1200

    Add suburb sync method

--- models/suburb.js
+++ models/suburb.js
@@ -55,6 +55,59 @@ suburbSchema.statics.upsert = function (config) {
 };
 
 /**
+ * Sync suburbs with the given API
+ *
+ * This will load all suburbs for the given region
+ *
+ * @method sync
+ * @param  {Object} config
+ *         @param {Integer}   config.region A region ID to sync suburbs for
+ *         @param {Interface} config.api    An instance of the trademe API interface
+ *         @param {Function}  config.callback
+ */
+suburbSchema.statics.sync = function (config) {
+    var api          = config.api,
+        regionId     = config.region,
+        callback     = config.callback,
+        Suburb       = this,
+        count        = 0,
+        totalSuburbs = 0,
+        handleResponse,
+        handleRegion;
+
+    handleResponse = function (regions) {
+        _.each(regions, function (thisRegion) {
+            // For each region (Northland, Auckland, Waikato, etc...)
+            if (thisRegion.LocalityId === regionId) {
+                // Find the region we are interested in
+                _.each(thisRegion.Districts, function (thisDistrict) {
+                    // For each district (Auckland City, Rodney, etc...)
+                    totalSuburbs += thisDistrict.Suburbs.length;
+                    _.each(thisDistrict.Suburbs, function (thisSuburb) {
+                        // For each suburb (City Centre, Manukau, Ponsonby, Grey Lynn, etc...)
+                        // Upsert the Suburb
+                        Suburb.upsert({
+                            id: thisSuburb.SuburbId,
+                            name: thisSuburb.Name,
+                            region: regionId,
+                            callback: function () {
+                                count++;
+
+                                if (count === totalSuburbs) {
+                                    callback();
+                                }
+                            }.bind(this)
+                        });
+                    }.bind(this));
+                }.bind(this));
+            }
+        }.bind(this));
+    };
+
+    api.getLocalities(handleResponse.bind(this));
+};
+
+/**
  * Export
  */
 module.exports = mongoose.model('Suburb', suburbSchema);
