904d3f57fbab42eb44f5ae350fc3aecaa6456483
Rowan Oulton <rowan.oulton@gmail.com>
2014-04-27 18:42:05 +1200

    Add suburb upsert method

--- models/suburb.js
+++ models/suburb.js
@@ -1,7 +1,8 @@
 /**
  * Dependencies
  */
-var mongoose = require('mongoose'),
+var _        = require('underscore'),
+    mongoose = require('mongoose'),
     Schema   = mongoose.Schema,
     suburbSchema;
 
@@ -23,6 +24,35 @@ suburbSchema = new Schema({
  * There should be only a single entry for each suburb, for each region.
  */
  suburbSchema.index({ _id: 1, _region: 1 }, { unique: true });
+
+/**
+* Statics
+*/
+
+/**
+ * Update (or insert) a suburb into the database
+ *
+ * @method upsert
+ * @param  {Object} config
+ *         @param {Number}   config.id         The suburb ID
+ *         @param {String}   config.name       The suburb name
+ *         @param {Number}   config.region     The region ID the suburb belongs to
+ *         @param {Function} [config.callback]
+ */
+suburbSchema.statics.upsert = function (config) {
+    var Suburb = this;
+
+    Suburb.update({ _id: config.id }, {
+        name: config.name,
+        _region: config.region
+    }, { upsert: true }, function (err) {
+        if (err) throw err;
+
+        if (typeof config.callback === 'function') {
+            config.callback();
+        }
+    }.bind(this));
+};
 
 /**
  * Export
