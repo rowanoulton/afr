24ea319c151393fb0689f89be7524c3beab88812
Rowan Oulton <rowan.oulton@gmail.com>
2014-04-30 09:04:57 +1200

    Region, suburb & statistic syncing process written into daemon script

--- afrd.js
+++ afrd.js
@@ -6,7 +6,10 @@ var fs            = require('fs'),
     _             = require('underscore'),
     Suburb        = require('./models/suburb'),
     Region        = require('./models/region'),
+    Statistics    = require('./models/stat'),
+    Series        = require('./lib/series'),
     Trademe       = require('./lib/trademe/interface'),
+    TrademeRegion = require('./lib/trademe/region'),
     Geocoder      = require('./lib/geocoder');
 
 /**
@@ -50,34 +53,95 @@ api = new Trademe({
     token: config.api.token
 });
 
-// Create and fetch each region specified in configuration
-_.each(config.regions, function (regionConfig) {
+var syncRegion,
+    syncSuburbs,
+    syncGeocodes,
+    syncStats;
+
+syncRegion = function (regionConfig) {
     Region.upsert({
         id: regionConfig.id,
         name: regionConfig.name,
-        callback: function () {
-            // Once the region has been upserted, sync suburbs for that region
-            // by polling the API for locality information, then looping each
-            // suburb for our given region and performing an upsert on each.
-            console.log(regionConfig.name + ' loaded, syncing suburbs...');
-            Suburb.sync({
-                api: api,
-                region: regionConfig.id,
-                callback: function () {
-                    var geocoder = new Geocoder({
-                        regionId: regionConfig.id
-                    });
+        callback: syncSuburbs.bind(this)
+    });
+};
 
-                    // At this point, the region and all it's suburbs are
-                    // present in the db, we can begin to load, process and store
-                    // statistical data
-                    console.log('Suburbs synced for ' + regionConfig.name);
+syncSuburbs = function (regionId) {
+    // Once the region has been upserted, sync suburbs for that region
+    // by polling the API for locality information, then looping each
+    // suburb for our given region and performing an upsert on each.
+    Suburb.sync({
+        api: api,
+        region: regionId,
+        callback: function (regionId) {
+            var regionConfig = _.findWhere(config.regions, { id: regionId });
 
-                    // Begin geocoding process to confirm that all suburbs have
-                    // coordinate values
-                    geocoder.start();
-                }.bind(this)
-            });
+            // At this point, the region and all it's suburbs are
+            // present in the db, we can begin to load, process and store
+            // statistical data
+            console.log('Suburbs synced for ' + regionConfig.name);
+            syncGeocodes(regionId);
+            syncStats(regionId);
         }.bind(this)
     });
-}.bind(this));
+};
+
+syncGeocodes = function (regionId) {
+    var geocoder = new Geocoder({
+        regionId: regionId
+    });
+
+    // Begin geocoding process to confirm that all suburbs have
+    // coordinate values
+    geocoder.start();
+};
+
+syncStats = function (regionId) {
+    var keys           = ['price', 'price_per_room'],
+        statsProcessed = 0,
+        statsToProcess = 0,
+        regionConfig,
+        region;
+
+    // Used to retrieve region name
+    regionConfig = _.findWhere(config.regions, { id: regionId });
+
+    // TrademeRegion handles the loading & sorting of rental listings
+    region = new TrademeRegion({
+        id: regionId,
+        api: api
+    });
+
+    // Fetch the listings data, then process them into series
+    region.fetch(function (collection) {
+        var suburbs = collection.getSortedBySuburb();
+
+        console.log(collection.length + ' listings loaded for ' + regionConfig.name);
+
+        _.each(suburbs, function (suburb, suburbId) {
+            var series = new Series(keys, suburb);
+
+            // @todo: Should this be pre-calculated with an initial pass over the suburbs list
+            statsToProcess++;
+
+            // Parse series into database models and save them
+            Statistics.fromSeries({
+                region: regionId,
+                suburb: suburbId,
+                series: series,
+                keys: keys,
+                callback: function () {
+                    statsProcessed++;
+
+                    if (statsProcessed === statsToProcess) {
+                        // We are finished, notify user
+                        console.log('Statistics saved for ' + regionConfig.name);
+                    }
+                }.bind(this)
+            });
+        }.bind(this));
+    }.bind(this));
+};
+
+// Create and fetch each region specified in configuration
+_.each(config.regions, syncRegion.bind(this));
