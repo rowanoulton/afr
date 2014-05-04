d64c5023f71d09901687a9194875e31f8420634e
Rowan Oulton <rowan.oulton@gmail.com>
2014-05-04 16:41:38 +1200

    Pruned some emoji out of logs Went a bit overboard there, holy moly

--- afrd.js
+++ afrd.js
@@ -107,7 +107,7 @@ syncSuburbs = function (regionId) {
             // At this point, the region and all it's suburbs are
             // present in the db, we can begin to load, process and store
             // statistical data
-            console.log('📚  -> Suburbs synced for ' + regionConfig.name);
+            console.log('Suburbs synced for ' + regionConfig.name);
             syncGeocodes(regionId, regionConfig.name);
             syncStats(regionId);
         }.bind(this)
@@ -160,7 +160,7 @@ syncStats = function (regionId) {
             // Log debug information about statistic processing to file to prevent pollution of console
             logger  = new FileLogger('stats');
 
-        console.log('📚  -> ' + collection.length + ' listings loaded for ' + regionConfig.name);
+        console.log(collection.length + ' listings loaded for ' + regionConfig.name);
 
         _.each(suburbs, function (suburb, suburbId) {
             var series = new Series(keys, suburb);
@@ -179,7 +179,7 @@ syncStats = function (regionId) {
 
                     if (statsProcessed === statsToProcess) {
                         // We are finished, notify user
-                        console.log('📊  -> Statistics saved for ' + regionConfig.name);
+                        console.log('Statistics saved for ' + regionConfig.name);
                     }
                 }.bind(this)
             });
