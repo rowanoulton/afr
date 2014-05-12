51355b8f447714b155dbe260d0f71b2b549c0f26
Rowan Oulton <rowan.oulton@gmail.com>
2014-05-13 08:52:26 +1200

    Unify regions/suburbs routes for now

--- routes/regions.js
+++ routes/regions.js
@@ -2,16 +2,31 @@
  * Dependencies
  */
 var Router = require('express').Router,
-    Region = require(__dirname + '/../models/region');
+    Region = require(__dirname + '/../models/region'),
+    Suburb = require(__dirname + '/../models/suburb');
 
 /**
  * Regions endpoint
  */
 router = new Router();
-router.get('/regions', function (req, res) {
+
+/**
+ * Region list
+ */
+router.get('/', function (req, res) {
     Region.find({}, { name: 1 }, function (err, regions) {
         if (err) throw err;
         res.send(regions);
+    });
+});
+
+/**
+ * Suburb list
+ */
+router.get('/:id/suburbs', function (req, res) {
+    Suburb.find({ _region: req.params.id }, { district: 1, name: 1, latitude: 1, longitude: 1 }, function (err, suburbs) {
+        if (err) throw err;
+        res.send(suburbs);
     });
 });
 
