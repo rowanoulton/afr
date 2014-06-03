09eaa1ed38b1448538993d8860d88488912a160f
Rowan Oulton <rowan.oulton@gmail.com>
2014-06-03 22:12:41 +1200

    Add gulp watcher, some other improvements to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,33 @@
  * Dependencies
  */
 var gulp       = require('gulp'),
-    jshint     = require('jshint'),
+    jshint     = require('gulp-jshint'),
     browserify = require('browserify'),
     source     = require('vinyl-source-stream');
 
+/**
+ * Setup
+ */
+var paths = {
+    scripts: './app/scripts/**/*.js',
+    browserify: './app/scripts/main.js'
+};
+
 gulp.task('lint', function () {
-    gulp.src('./app/scripts/*.js')
+    return gulp.src(paths.scripts)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('browserify', function() {
-    return browserify('./app/scripts/main.js')
+    return browserify(paths.browserify)
         .bundle()
         .pipe(source('afr.js'))
         .pipe(gulp.dest('./public/assets/js'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['lint', 'browserify']);
+});
+
+gulp.task('default', ['watch']);
