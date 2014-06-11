1860ef6c7dcc44589d69a33dc9e14f254b8066fe
Rowan Oulton <rowan.oulton@gmail.com>
2014-06-11 22:03:22 +1200

    Handle is-selected class switching in nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,10 +7,15 @@ var app = angular.module('afrApp', []);
 
 app.controller('RegionCtrl', function($scope, $http) {
   $http.get('/regions').success(function (data) {
-    $scope.regions = data;
+    $scope.regions  = data;
+    $scope.selected = $scope.regions[0];
   });
 
   $scope.select = function (region) {
+    $scope.selected = region;
+  };
 
+  $scope.getClass = function (region) {
+    return region === $scope.selected ? 'is-selected' : '';
   };
 });
