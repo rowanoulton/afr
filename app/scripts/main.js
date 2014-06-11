f4849444e6ec0ada12ea39227eec00df2e16f8a3
Rowan Oulton <rowan.oulton@gmail.com>
2014-06-11 22:21:46 +1200

    Rename `selected` to `selectedRegion` To be more explanatory

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,14 +8,14 @@ var app = angular.module('afrApp', []);
 app.controller('RegionCtrl', function($scope, $http) {
   $http.get('/regions').success(function (data) {
     $scope.regions  = data;
-    $scope.selected = $scope.regions[0];
+    $scope.selectedRegion = $scope.regions[0];
   });
 
   $scope.select = function (region) {
-    $scope.selected = region;
+    $scope.selectedRegion = region;
   };
 
   $scope.getClass = function (region) {
-    return region === $scope.selected ? 'is-selected' : '';
+    return region === $scope.selectedRegion ? 'is-selected' : '';
   };
 });
