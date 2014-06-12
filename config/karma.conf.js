dc8c8314a19ba86ead225bb0ab4fe75cb5d4e4f9
Rowan Oulton <rowan.oulton@gmail.com>
2014-06-12 18:39:27 +1200

    Use compiled scripts in karma test

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/scripts/**/*.js',
+      'public/assets/js/**/*.js',
       'app/test/**/*.js'
     ],
 
