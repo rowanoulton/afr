07978d8404c4a7684260d85e304a7202f5eeea87
Rowan Oulton <rowan.oulton@gmail.com>
2014-06-12 22:02:09 +1200

    Add suburb controller unit test

---
+++ app/test/controllers/suburbCtrl.js
@@ -0,0 +1,106 @@
+'use strict';
+
+describe('SuburbCtrl', function () {
+    var $httpBackend,
+        $rootScope,
+        $controller,
+        createController,
+        selectedRegion,
+        suburbs;
+
+    beforeEach(module('afrApp'));
+
+    beforeEach(inject(function ($injector) {
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope   = $injector.get('$rootScope');
+        $controller  = $injector.get('$controller');
+
+        selectedRegion = {
+            id: 1,
+            name: 'TestRegion'
+        };
+
+        suburbs = [
+            {
+                id: 1,
+                name: 'TestSuburb'
+            },
+            {
+                id: 2,
+                name: 'TestSuburb2'
+            }
+        ];
+
+        $httpBackend
+            .expectGET('/regions/1/suburbs')
+            .respond(suburbs);
+
+        createController = function () {
+            return $controller('SuburbCtrl', { $scope: $rootScope });
+        };
+    }));
+
+    it('should have no suburbs defined until a region is selected', function () {
+        var controller = createController();
+
+        expect($rootScope.suburbs).toBeUndefined();
+        expect($rootScope.selectedSuburb).toBeUndefined();
+    });
+
+    it('should listen for changes to selectedRegion and only request suburbs from server when a region is selected', function() {
+        var controller = createController();
+
+        $rootScope.$apply(function () {
+            $rootScope.selectedRegion = selectedRegion;
+        });
+
+        $httpBackend.flush();
+
+        $httpBackend.verifyNoOutstandingExpectation();
+        expect($rootScope.suburbs).toBeDefined();
+        expect($rootScope.suburbs).toEqual(suburbs);
+    });
+
+    it('should select the first returned suburb', function() {
+        var controller = createController();
+
+        $rootScope.$apply(function () {
+            $rootScope.selectedRegion = selectedRegion;
+        });
+
+        $httpBackend.flush();
+
+        expect($rootScope.selectedSuburb).toEqual(suburbs[0]);
+    });
+
+    it('should not request suburbs when a falsey region is selected', function() {
+        var controller = createController();
+
+        $rootScope.$apply(function () {
+            $rootScope.selectedRegion = false;
+        });
+
+        $httpBackend.verifyNoOutstandingRequest();
+
+        $rootScope.$apply(function () {
+            $rootScope.selectedRegion = null;
+        });
+
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should updated the suburb selection when a suburb is selected', function() {
+        var controller = createController();
+
+        $rootScope.$apply(function () {
+            $rootScope.selectedRegion = selectedRegion;
+        });
+
+        $httpBackend.flush();
+
+        $rootScope.select(suburbs[1]);
+        expect($rootScope.selectedSuburb).toEqual(suburbs[1]);
+        $rootScope.select(suburbs[0]);
+        expect($rootScope.selectedSuburb).toEqual(suburbs[0]);
+    });
+});
